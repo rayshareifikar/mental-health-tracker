e3a2fcafd1c678b9bb779e29324c724eff511c51
rayshareifikar <nyananyana23@gmail.com>
2024-09-11 14:39:43 +0700

    Tutorial 2 Done!

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+
 class MoodEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
